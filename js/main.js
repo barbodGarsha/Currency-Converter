d603fc5370010f7c8f2fa24bc2e5885b408d9e1a
Barbod Garsha <barbodgarshasbi79@gmail.com>
2022-02-04 17:50:11 +0100

    getting the currencies and adding them to the dropdown menu

--- js/main.js
+++ js/main.js
@@ -1,30 +1,32 @@
+const currencies_options = document.getElementsByClassName("currencies-options")
+console.log(currencies_options)
+
+//API----------------------------------------------------------
 const frankfurter_api_url = 'https://api.frankfurter.app'
 
-async function get_api(url) {
-  const response = await fetch(url);
-  const data = await response.json();
-  
-  console.log(data);
-}
-
-
 async function get_currencies() {
-  const response = await fetch(frankfurter_api_url + '/currencies');
-  const data = await response.json();
+  const response = await fetch(frankfurter_api_url + '/currencies')
+  const data = await response.json()
   
+  let count = 0;
   for (var key in data) {
-    console.log('name=' + key + ' value=' + data[key])
+    const option = document.createElement('option')
+    option.value = key
+    option.innerText = data[key] + " (" + key + ")" 
+    for (const element of currencies_options) {
+      element.appendChild(option.cloneNode(true)) 
+    }
+    count++
   }
 }
 
 get_currencies()
 
 async function convert_currencies(c1, c2, amount) {
-  let result;
+  let result
   const response = await fetch(`${frankfurter_api_url}/latest?amount= ${amount}&from=${c1}&to=${c2}`)
   const data = await response.json()
   
-  console.log(`${amount} ${c1} = ${data.rates[c2]} ${c2}`);
+  console.log(`${amount} ${c1} = ${data.rates[c2]} ${c2}`)
 }
 
-convert_currencies('USD', 'EUR', 20)
