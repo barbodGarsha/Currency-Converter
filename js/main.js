f43b9e14e24be5a59f0b739e60d603e3d33afb4f
Barbod Garsha <barbodgarshasbi79@gmail.com>
2022-02-04 12:47:31 +0100

    setup the Frankfurter API

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const frankfurter_api_url = 'https://api.frankfurter.app'
+
+async function get_api(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  
+  console.log(data);
+}
+
+
+async function get_currencies() {
+  const response = await fetch(frankfurter_api_url + '/currencies');
+  const data = await response.json();
+  
+  for (var key in data) {
+    console.log(' name=' + key + ' value=' + data[key])
+  }
+}
+
+get_currencies()
+
+async function convert_currencies(c1, c2, amount) {
+  let result;
+  const response = await fetch(`${frankfurter_api_url}/latest?amount= ${amount}&from=${c1}&to=${c2}`)
+  const data = await response.json()
+  
+  console.log(`${amount} ${c1} = ${data.rates[c2]} ${c2}`);
+}
+
+convert_currencies('USD', 'EUR', 20)
